900188b2814f24a80162a8a121baab9f6b0cad88
Jeffrey Chrabaszcz <jchrabaszcz@gmail.com>
2017-08-21 11:48:13 -0400

    added probability of retrival plot

--- app.R
+++ app.R
@@ -85,6 +85,9 @@ ui <- fluidPage(
       ),
       fluidRow(
         column(12, ggvisOutput("actsPlot"))
+      ),
+      fluidRow(
+        column(12, ggvisOutput("probPlot"))
       )
     )
   )
@@ -109,9 +112,10 @@ server <- function(input, output) {
     mem[names(mem) == pp] <- 1
     ns <- matrix(runif(trial * 6), ncol = 6)
     # ns <- t(apply(ns, 1, function(x) x * (c(pa, (1 - pa), 1, pb, (1 - pb), 1) != 0)))
-    out <- rep(NA, trial)
+    out <- rep(NA, trial) # simplex probability of choosing option a
     act_out <- matrix(rep(NA, 4 * trial), ncol = 4)
     bv_out <- matrix(rep(NA, 2 * trial), ncol = 2)
+    pr_out <- matrix(rep(NA, 4 * trial), ncol = 4)
     out[1] <- 1 / 2
     for (t in 2:trial) {
       noise <- ns[t, ]
@@ -119,7 +123,9 @@ server <- function(input, output) {
       # acts <- ifelse(acts == Inf, -Inf, acts)
       acts <- ifelse(c(pa, (1 - pa), 1, pb, (1 - pb), 1) == 0, -Inf, acts)
       act_out[t,] <- acts[c(1:2, 4:5)]
+      
       ps <- c(exp(acts[1:3] / tau) / sum(exp(acts[1:3] / tau)),  exp(acts[4:6] / tau) / sum(exp(acts[4:6] / tau)))
+      pr_out[t,] <- ps[c(1:2,4:5)]
       
       vals <- c(ps[1:3] %*% c(v_a1, v_a2, pp), ps[4:6] %*% c(v_b1, v_b2, pp))
       bv_out[t,] <- vals
@@ -132,12 +138,12 @@ server <- function(input, output) {
       )
       mem[names(mem) == tmp][[1]] <- c(mem[names(mem) == tmp][[1]], t)
     }
-    tmp <- data.frame(cbind(idx, out, act_out, bv_out, 1:trial))
-    names(tmp) <- c("idx", "out", "aa_1", "aa_2", "ab_1", "ab_2", "bv_a", "bv_b", "trial")
+    tmp <- data.frame(cbind(idx, out, act_out, bv_out, 1:trial, pr_out))
+    names(tmp) <- c("idx", "out", "aa_1", "aa_2", "ab_1", "ab_2", "bv_a", "bv_b", "trial", "pa_1", "pa_2", "pb_1", "pb_2")
     tmp
   }
   
-  values <- reactiveValues(hot = init)
+  values <- reactiveValues(hot = init) # table setup based on initial values at top
   
   output$hot = renderRHandsontable({
     DT = NULL
@@ -206,6 +212,15 @@ server <- function(input, output) {
       mutate(opts = ifelse(opts == "aa_1", "Option A, Outcome 1", ifelse(opts == "aa_2", "Option A, Outcome 2", ifelse(opts == "ab_1", "Option B, Outcome 1", "Option B, Outcome 2"))))
   })
   
+  pr_dat <- reactive({
+    p_dat() %>%
+      dplyr::select(idx, pa_1:pb_2, trial) %>%
+      dplyr::filter(trial > 1) %>%
+      gather(opts, acts, -trial, -idx) %>%
+      filter(acts > -Inf) %>%
+      mutate(opts = ifelse(opts == "pa_1", "Option A, Outcome 1", ifelse(opts == "pa_2", "Option A, Outcome 2", ifelse(opts == "pb_1", "Option B, Outcome 1", "Option B, Outcome 2"))))
+  })
+  
   bv_dat %>% ggvis(~trial, ~bv) %>%
     group_by(Option) %>%
     layer_smooths(stroke = ~Option, se = TRUE) %>%
@@ -224,6 +239,15 @@ server <- function(input, output) {
     add_axis("y", title = "Activation") %>%
     add_axis("x", title = "Trial") %>%
     bind_shiny("actsPlot")
+  
+  pr_dat %>% ggvis(~trial, ~acts) %>%
+    group_by(opts) %>%
+    layer_smooths(stroke = ~opts, fill = ~opts, se = TRUE) %>%
+    # layer_points(fill = ~opts, opacity := .5) %>%
+    add_axis("y", title = "Probability of Retrieval") %>%
+    add_axis("x", title = "Trial") %>%
+    scale_numeric("y", domain = c(0, 1), nice = TRUE) %>%
+    bind_shiny("probPlot")
 }
 
 # Run the application 
